9cad6ade67812db14177adbcbcbac493baa48722
stewart morales <stewartmorales129@gmail.com>
2021-02-02 17:37:31 -0500

    finish phase 0 and halfway in phase 1

---
+++ myActiveRecord/lib/01_sql_object.rb
@@ -0,0 +1,74 @@
+require_relative 'db_connection'
+require 'active_support/inflector'
+# NB: the attr_accessor we wrote in phase 0 is NOT used in the rest
+# of this project. It was only a warm up.
+
+class SQLObject
+  def self.columns
+    return @column if @column
+    rows = DBConnection.execute2(<<-SQL)
+      SELECT 
+        *
+      FROM
+        #{self.table_name}
+    SQL
+
+    @column = rows.first.map(&:to_sym)
+  end
+
+  def self.finalize!
+    self.columns.each do |col|
+      define_method("#{col}") do 
+        self.attributes[col]
+      end
+      define_method("#{col}=") do |val|
+        self.attributes[col] = val
+      end
+    end
+  end
+
+  def self.table_name=(table_name)
+    @table_name = table_name
+  end
+
+  def self.table_name
+    @table_name ||= self.to_s.tableize
+  end
+
+  def self.all
+    # ...
+  end
+
+  def self.parse_all(results)
+    # ...
+  end
+
+  def self.find(id)
+    # ...
+  end
+
+  def initialize(params = {})
+    
+  end
+
+  def attributes
+    @attributes ||= {}
+  end
+
+  def attribute_values
+    # ...
+  end
+
+  def insert
+    # ...
+  end
+
+  def update
+    # ...
+  end
+
+  def save
+    # ...
+  end
+end
+
